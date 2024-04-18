b5b191a46dbcc652b078159e97367cbffd94c907
Pietras33 <166720054+Pietras33@users.noreply.github.com>
2024-04-18 14:00:18 +0200

    feat: add create users

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utils.crud import read
+from utils.crud import read, create_user
 from models.data import users
 
 
@@ -10,8 +10,11 @@ if __name__ == '__main__':
         print("menu:")
         print("0.zakoncz program")
         print("1.pokaz co u znajomych")
+        print("2.dodaj znajomego")
         menu_option:str=input("wybierz dostepna funkcje z menu:")
         if menu_option == "0":
             break
         if menu_option == "1":
           read(users)
+        if menu_option == "2":
+            create_user(users)
--- utils/crud.py
+++ utils/crud.py
@@ -1,5 +1,12 @@
-import main
+
 def read(users: list[dict]) -> None:
     for user in users[1:]:
         print(f'Twoj znajomy {user["name"]} opublikował: {user["posts"]}')
+
+def create_user(user:list[dict])->None:
+    name: str = input("podaj imie uzytkownika:")
+    surname: str = input("podaj nazwisko uzytkownika:")
+    posts: int = int(input("podaj liczbe postow:"))
+    user: dict = {"name": name, "surname": surname, "posts": posts}
+    users.append(user)
 
